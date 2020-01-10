7eefa240c3962c614d3cb9b5c9a5d8dca671254d
Alex Baranowski <alex@euro-linux.com>
2020-01-10 08:13:25 +0100

    els-ASAP auto commit file/tag libmusicbrainz5-5.0.1-7.el7.src.rpm

---
+++ src/src/ISWC.cc
@@ -0,0 +1,123 @@
+/* --------------------------------------------------------------------------
+
+   libmusicbrainz5 - Client library to access MusicBrainz
+
+   Copyright (C) 2012 Andrew Hawkins
+
+   This file is part of libmusicbrainz5.
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of v2 of the GNU Lesser General Public
+   License as published by the Free Software Foundation.
+
+   libmusicbrainz5 is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
+     $Id$
+
+----------------------------------------------------------------------------*/
+
+#include "config.h"
+
+#include "musicbrainz5/ISWC.h"
+
+
+class MusicBrainz5::CISWCPrivate
+{
+	public:
+		CISWCPrivate()
+		{
+		}
+
+		std::string m_ISWC;
+};
+
+MusicBrainz5::CISWC::CISWC(const XMLNode& Node)
+:	CEntity(),
+	m_d(new CISWCPrivate)
+{
+	if (!Node.isEmpty())
+	{
+		//std::cout << "ISWC node: " << std::endl << Node.createXMLString(true) << std::endl;
+
+		Parse(Node);
+
+		if (Node.getText())
+			ProcessItem(Node,m_d->m_ISWC);
+	}
+}
+
+MusicBrainz5::CISWC::CISWC(const CISWC& Other)
+:	CEntity(),
+	m_d(new CISWCPrivate)
+{
+	*this=Other;
+}
+
+MusicBrainz5::CISWC& MusicBrainz5::CISWC::operator =(const CISWC& Other)
+{
+	if (this!=&Other)
+	{
+		Cleanup();
+
+		CEntity::operator =(Other);
+
+		m_d->m_ISWC=Other.m_d->m_ISWC;
+	}
+
+	return *this;
+}
+
+MusicBrainz5::CISWC::~CISWC()
+{
+	Cleanup();
+
+	delete m_d;
+}
+
+void MusicBrainz5::CISWC::Cleanup()
+{
+}
+
+MusicBrainz5::CISWC *MusicBrainz5::CISWC::Clone()
+{
+	return new CISWC(*this);
+}
+
+void MusicBrainz5::CISWC::ParseAttribute(const std::string& Name, const std::string& /*Value*/)
+{
+	std::cerr << "Unrecognised ISWC attribute: '" << Name << "'" << std::endl;
+}
+
+void MusicBrainz5::CISWC::ParseElement(const XMLNode& Node)
+{
+	std::string NodeName=Node.getName();
+
+	std::cerr << "Unrecognised ISWC element: '" << NodeName << "'" << std::endl;
+}
+
+std::string MusicBrainz5::CISWC::GetElementName()
+{
+	return "iswc";
+}
+
+std::string MusicBrainz5::CISWC::ISWC() const
+{
+	return m_d->m_ISWC;
+}
+
+std::ostream& MusicBrainz5::CISWC::Serialise(std::ostream& os) const
+{
+	os << "ISWC:" << std::endl;
+
+	CEntity::Serialise(os);
+
+	os << "\tISWC: " << ISWC() << std::endl;
+
+	return os;
+}
---
+++ src/src/RelationList.cc
@@ -0,0 +1,111 @@
+/* --------------------------------------------------------------------------
+
+   libmusicbrainz5 - Client library to access MusicBrainz
+
+   Copyright (C) 2012 Andrew Hawkins
+
+   This file is part of libmusicbrainz5.
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of v2 of the GNU Lesser General Public
+   License as published by the Free Software Foundation.
+
+   libmusicbrainz5 is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
+     $Id$
+
+----------------------------------------------------------------------------*/
+
+#include "config.h"
+#include "musicbrainz5/defines.h"
+
+#include "musicbrainz5/RelationList.h"
+
+#include "musicbrainz5/Relation.h"
+
+class MusicBrainz5::CRelationListPrivate
+{
+	public:
+		std::string m_TargetType;
+};
+
+MusicBrainz5::CRelationList::CRelationList(const XMLNode& Node)
+:	CListImpl<CRelation>(),
+	m_d(new CRelationListPrivate)
+{
+	if (!Node.isEmpty())
+	{
+		//std::cout << "RelationList node: " << std::endl << Node.createXMLString(true) << std::endl;
+
+		Parse(Node);
+	}
+}
+
+MusicBrainz5::CRelationList::CRelationList(const CRelationList& Other)
+:	CListImpl<CRelation>(),
+	m_d(new CRelationListPrivate)
+{
+	*this=Other;
+}
+
+MusicBrainz5::CRelationList& MusicBrainz5::CRelationList::operator =(const CRelationList& Other)
+{
+	if (this!=&Other)
+	{
+		CListImpl<CRelation>::operator =(Other);
+
+		m_d->m_TargetType=Other.m_d->m_TargetType;
+	}
+
+	return *this;
+}
+
+MusicBrainz5::CRelationList::~CRelationList()
+{
+	delete m_d;
+}
+
+MusicBrainz5::CRelationList *MusicBrainz5::CRelationList::Clone()
+{
+	return new CRelationList(*this);
+}
+
+void MusicBrainz5::CRelationList::ParseAttribute(const std::string& Name, const std::string& Value)
+{
+	if ("target-type"==Name)
+		ProcessItem(Value,m_d->m_TargetType);
+	else
+		CListImpl<CRelation>::ParseAttribute(Name,Value);
+}
+
+void MusicBrainz5::CRelationList::ParseElement(const XMLNode& Node)
+{
+	CListImpl<CRelation>::ParseElement(Node);
+}
+
+std::string MusicBrainz5::CRelationList::GetElementName()
+{
+	return "relation-list";
+}
+
+std::string MusicBrainz5::CRelationList::TargetType() const
+{
+	return m_d->m_TargetType;
+}
+
+std::ostream& MusicBrainz5::CRelationList::Serialise(std::ostream& os) const
+{
+	os << "Relation list:" << std::endl;
+
+	os << "\tTarget type: " << TargetType() << std::endl;
+
+	CListImpl<CRelation>::Serialise(os);
+
+	return os;
+}
